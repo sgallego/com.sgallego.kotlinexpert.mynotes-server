4dcce3f92f6b865bc3851aed54b53c5f0ca6a5da
Sebastián Gallego <sebastian.gallego.c@gmail.com>
2024-03-06 16:40:41 +0100

    6.4: Basic navigation

--- src/main/kotlin/com/sgallego/plugins/Routing.kt
+++ src/main/kotlin/com/sgallego/plugins/Routing.kt
@@ -1,13 +1,40 @@
 package com.sgallego.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.html.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
+        get("") {
             call.respondText("Hello World!")
+        }
+        get("/html") {
+            call.respondHtml(status = HttpStatusCode.OK) {
+                head {
+                    title { +"Hello Ktor" }
+                }
+                body {
+                    h1 { +"This is an h1" }
+                    p { +"This is a paragraph" }
+                    div {
+                        a(href = "html/clicked") { +"Click me" }
+                    }
+                }
+            }
+        }
+        get("/html/clicked") {
+            call.respondHtml {
+                head {
+                    title { +"Clicked" }
+                }
+                body {
+                    h1 { +"Button clicked" }
+                }
+            }
         }
     }
 }
