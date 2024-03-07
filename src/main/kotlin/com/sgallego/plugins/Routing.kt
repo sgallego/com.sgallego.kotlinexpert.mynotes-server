903191c72ab39b3017c4cdeeca8fcaeebe3571ae
Sebastián Gallego <sebastian.gallego.c@gmail.com>
2024-03-07 10:48:17 +0100

    6.10: CRUD functions routing

--- src/main/kotlin/com/sgallego/plugins/Routing.kt
+++ src/main/kotlin/com/sgallego/plugins/Routing.kt
@@ -1,7 +1,10 @@
 package com.sgallego.plugins
 
+import com.sgallego.models.Note
 import com.sgallego.repositories.NotesRepository
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -9,8 +12,70 @@ fun Application.configureRouting() {
     routing {
         route("/notes"){
             //CREATE
+            post {
+                try {
+                    val note = call.receive<Note>()
+                    val savedNote = NotesRepository.save(note)
+                    call.respond(HttpStatusCode.Created, savedNote)
+                }catch (e: Exception){
+                    call.respond(
+                        HttpStatusCode.BadRequest,
+                        "Bad JSON Data Body: ${e.message}")
+                }
+            }
+
+            //READ
             get {
                 call.respond(NotesRepository.getAll())
+            }
+            get("{id}") {
+                val id = call.parameters["id"] ?: return@get call.respond(
+                    HttpStatusCode.BadRequest,
+                    "Missing or malformed id"
+                )
+                val note = NotesRepository.getById(id.toLong()) ?: return@get call.respond(
+                    HttpStatusCode.NotFound,
+                    "No note with id $id"
+                )
+
+                call.respond(note)
+
+            }
+
+            //UPDATE
+            put {
+                try {
+                    val note = call.receive<Note>()
+                    if( NotesRepository.update(note)){
+                        call.respond(note)
+                    }else{
+                        call.respond(
+                            HttpStatusCode.NotFound,
+                            "No note with id ${note.id}"
+                        )
+                    }
+
+                }catch (e: Exception){
+                    call.respond(
+                        HttpStatusCode.BadRequest,
+                        "Bad JSON Data Body: ${e.message}")
+                }
+            }
+
+            //DELETE
+            delete("{id}") {
+                val id = call.parameters["id"] ?: return@delete call.respond(
+                    HttpStatusCode.BadRequest,
+                    "Missing or malformed id"
+                )
+                if( NotesRepository.delete(id.toLong())){
+                    call.respond(HttpStatusCode.Accepted)
+                }else{
+                    call.respond(
+                        HttpStatusCode.NotFound,
+                        "No note with id ${id}"
+                    )
+                }
             }
         }
 
